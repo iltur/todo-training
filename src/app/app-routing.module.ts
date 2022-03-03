7e4d5ed9204617bfbbcc199a65e4a51eb22ab335
iltur <chmielecka.joanna@gmail.com>
2022-03-03 18:25:03 +0100

    kolejny commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageModule } from './pages/home.page-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ 
+        path: 'Home', 
+        loadChildren: () => HomePageModule
+      },
+  { 
+        path: '', 
+        loadChildren: () => HomePageModule
+      }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
