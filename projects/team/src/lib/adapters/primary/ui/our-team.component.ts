b75489942c300bc60f0a45aa21955cbbefd93a59
iltur <chmielecka.joanna@gmail.com>
2022-03-11 15:10:22 +0100

    Commit naprawa routerlinka w ramach componenta

--- projects/team/src/lib/adapters/primary/ui/our-team.component.ts
+++ projects/team/src/lib/adapters/primary/ui/our-team.component.ts
@@ -1,5 +1,13 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core' ;
+import { ActivatedRoute } from '@angular/router';
 
-@Component({ selector: 'lib-our-team', templateUrl: './our-team.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+@Component({ 
+    selector: 'lib-our-team', 
+    templateUrl: './our-team.component.html',
+    encapsulation: ViewEncapsulation.None, 
+    changeDetection: ChangeDetectionStrategy.OnPush })
 export class OurTeamComponent {
+    params$ = this._activatedRoute.params;
+    constructor(private _activatedRoute: ActivatedRoute) {}
+    
 }
