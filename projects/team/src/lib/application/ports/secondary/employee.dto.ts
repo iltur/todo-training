cc0f0ea06ad575dff26cb296b9b7864270679c09
iltur <chmielecka.joanna@gmail.com>
2022-03-14 12:38:05 +0100

    first DTO commit :)

---
+++ projects/team/src/lib/application/ports/secondary/employee.dto.ts
@@ -0,0 +1,9 @@
+import { Url } from "url";
+
+export interface EmployeeDTO {
+  readonly image: string;
+  readonly alt: string;
+  readonly name: string;
+  readonly position: string;
+  readonly detail: string;
+}
