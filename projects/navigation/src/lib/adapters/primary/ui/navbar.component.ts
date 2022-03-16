06e64359e0eb805698deb7eef076d9a8cfb0c407
iltur <chmielecka.joanna@gmail.com>
2022-03-16 15:56:10 +0100

    przeniesienie nawigacji do komponentu

---
+++ projects/navigation/src/lib/adapters/primary/ui/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+    selector: 'lib-navbar',
+    templateUrl: './navbar.component.html',
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NavbarComponent {
+
+
+}
---
+++ projects/navigation/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of navigation
+ */
+
+
+export * from './lib/adapters/primary/ui/navbar.component-module';
